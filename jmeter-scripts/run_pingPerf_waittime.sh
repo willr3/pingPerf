09308c5f9610e34b49d4098e340034159bef6a00
jdmcclur <jdmcclur@us.ibm.com>
2024-11-14 10:10:34 -0500

    add jmx script with wait time

---
+++ jmeter-scripts/run_pingPerf_waittime.sh
@@ -0,0 +1,15 @@
+JMETER_HOME=${1}
+SUT=${2}
+
+sleep 5
+THREADS=${3}
+
+${JMETER_HOME}/bin/jmeter.sh -n -t simple.jmx -JHOST=${SUT} -JPORT=9090 -JTHREAD=${THREADS} -JDURATION=30 -JURL=/ping/greeting
+sleep 5
+${JMETER_HOME}/bin/jmeter.sh -n -t simple.jmx -JHOST=${SUT} -JPORT=9090 -JTHREAD=${THREADS} -JDURATION=60 -JURL=/ping/greeting
+sleep 5
+${JMETER_HOME}/bin/jmeter.sh -n -t simple.jmx -JHOST=${SUT} -JPORT=9090 -JTHREAD=${THREADS} -JDURATION=180 -JURL=/ping/greeting
+./getFootprint.sh ${SUT}
+sleep 5
+${JMETER_HOME}/bin/jmeter.sh -n -t simple.jmx -JHOST=${SUT} -JPORT=9090 -JTHREAD=${THREADS} -JDURATION=300 -JURL=/ping/greeting
+
