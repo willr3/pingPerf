9b88c0afafcb67ec5612bbfcde22fd305eb5fba8
Will Reichert <willr3@gmail.com>
2024-12-12 14:02:21 -0500

    add --no-cache to pingperf-semeru-base

--- build.semeru.sh
+++ build.semeru.sh
@@ -1,4 +1,4 @@
-podman build -f Dockerfile.quarkus.semeru.base --cap-add=ALL --secret id=criu_secrets,src=.env -t pingperf-semeru-base .
+podman build -f Dockerfile.quarkus.semeru.base --cap-add=ALL --secret id=criu_secrets,src=.env -t pingperf-semeru-base --no-cache .
 podman build -f Dockerfile.quarkus.semeru --cap-add=ALL --security-opt seccomp=unconfined --cpuset-cpus 2 -m 1g -t pingperf-semeru-1cpu --no-cache .
 podman build -f Dockerfile.quarkus.semeru --cap-add=ALL --security-opt seccomp=unconfined --cpuset-cpus 2-3 -m 1g -t pingperf-semeru-2cpu --no-cache .
 podman build -f Dockerfile.quarkus.semeru --cap-add=ALL --security-opt seccomp=unconfined --cpuset-cpus 2-5 -m 1g -t pingperf-semeru-4cpu --no-cache .
